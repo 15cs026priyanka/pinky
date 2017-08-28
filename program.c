4f1d2519e60f292ab0c855aa29fbcdc800e65a58
15cs026priyanka <priyankaappu1998@gmail.com>
2017-08-28 12:27:55 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the value of a");
+scanf("%d",&a);
+if(a<=5)
+{
+printf("the given number is postive");
+}
+else
+{
+printf("the given number is negative");
+}
+getch();
+}
